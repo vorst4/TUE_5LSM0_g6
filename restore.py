e7933d464930d5df1376fac339ab3369e48acc0b
vorst4 <d.m.n.v.d.vorst@student.tue.nl>
2020-04-29 10:03:54 +0000

    updated restor.py such that a model from a gpu-device can be loded onto cpu-device

--- restore.py
+++ restore.py
@@ -25,7 +25,7 @@ def restore(model,
             model_path='/content/drive/My Drive/5LSM0-final-assignment/',
             date_time=''):
   
-  # if no date_time is given, use the latest
+  # construct path, if no date_time is given get most recent model
   if date_time == '':
     models = glob(model_path + modelname + '*.pt')
     if len(models) == 0:
@@ -38,10 +38,15 @@ def restore(model,
       print('\nERROR: Path of provided modelname and date_time not found\n')
       return model
 
-  # restore the model using the path
-  model.load_state_dict(torch.load(path))
-  model.eval()
-  print('\nRestored model\n')
+  # restore the model
+  if torch.cuda.is_available():
+    model.load_state_dict(torch.load(path))
+    model.eval()
+    print('\nRestored model\n')
+  else:
+    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    model.eval()
+  print('\nWARNING: Using CPU !!!\nRestored model\n')
 
   return model
   
